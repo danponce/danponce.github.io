f2a558ce92b5015c0321fdc5af68c03776037e7f
danponce <dponce6@gmail.com>
2015-10-21 21:23:51 -0300

    Cambios a login

---
+++ assets/js/index.login.js
@@ -0,0 +1,27 @@
+//$(function() {
+
+    //Parse.$ = jQuery;
+
+    Parse.initialize("ByL6KsOLSVjwIPsI5GEQ5ZKNTYsxDFsV5Ln6Ym93", "cZINkIPn1D5UA6apOQWghmm4pzIdZyxhB3jGxZ82");
+
+        /*var TestObject = Parse.Object.extend("TestObject");
+        var testObject = new TestObject();
+          testObject.save({foo: "bar"}, {
+          success: function(object) {
+            $(".success").show();
+          },
+          error: function(model, error) {
+            $(".error").show();
+          }
+        });*/
+
+    var currentUser = Parse.User.current();
+    if (currentUser) {
+        // do stuff with the user
+    } else {
+        // show the signup or login page
+        //alert("No esta logeado!");
+        window.location.href = "login.html";
+    }
+    
+//});
---
+++ assets/js/login.js
@@ -0,0 +1,37 @@
+$(function() {
+
+    //Parse.$ = jQuery;
+
+	Parse.initialize("ByL6KsOLSVjwIPsI5GEQ5ZKNTYsxDFsV5Ln6Ym93", "cZINkIPn1D5UA6apOQWghmm4pzIdZyxhB3jGxZ82");
+
+	
+
+	$('.form-login').on('submit', function(e) {
+ 
+	    // Prevent Default Submit Event
+	    e.preventDefault();
+	   
+        var username = $("#user").val();
+        var password = $("#pass").val();
+        
+       
+
+		Parse.User.logIn(username, password, {
+		  success: function(user) {
+		    // Do stuff after successful login.
+              alert("Login exitoso!!");
+		    window.location.href = "index.html";
+		    
+		  },
+		  error: function(user, error) {
+		    // The login failed. Check error to see why.
+		    alert("Error de Parse : " + error.message);
+		  }
+		});
+
+	 	
+	});    
+
+    
+});
+
